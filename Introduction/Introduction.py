d7faa88a18344555c3a6685117da7ab2d508f35e
Angelo-Angeles <144931031+Angelo-Angeles@users.noreply.github.com>
2023-09-13 19:35:22 +0400

    Intro
    This is my introduction. yaaayyyy

---
+++ Introduction/Introduction.py
@@ -0,0 +1,14 @@
+print('''My goal here is to make it interactable... 
+However it doesn't introduce me so I'll just intoduce myself here.
+I am Angelo Ivan Lemuel R Angeles. 
+I like programming and gaming that is all.''')
+
+name = input("""What is your Name?: 
+""")
+
+
+intro = input("""Introduce yourself: 
+""")
+
+print(name)
+print(intro)
